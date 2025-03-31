6eea37c4a036495a94e65773d54d520c4b380d25
znt10 <jc970807@gmail.com>
2025-03-31 09:57:51 -0300

    mm

--- teste.py
+++ teste.py
@@ -5,14 +5,14 @@ def busca_linear(lista, alvo):
     return -1
 
 
-
+#melhor caso O(1)
 numeros_pequenos = [15, 10, 5, 20, 25]
 alvo = 15
 resultado = busca_linear(numeros_pequenos, alvo)
 print(f"Resultado: {resultado}")
 
 
-
+#medio caso O(n)
 numeros_medios = list(range(1, 51))  # Lista de 1 a 50
 alvo = 30
 resultado = busca_linear(numeros_medios, alvo)
@@ -20,17 +20,16 @@ print(f"Resultado: {resultado}")
 
 
 
-
+#pior caso O(n)
 numeros_grandes = list(range(1, 1001))  # Lista de 1 a 1000
 alvo = 999
 resultado = busca_linear(numeros_grandes, alvo)
 print(f"Resultado: {resultado}")
 
-
+#pior caso O(n)
 numeros_mega_grande = list(range(1, 100001))  # Lista de 1 a 100000
 alvo = 100001  # Valor que não está na lista
 resultado = busca_linear(numeros_mega_grande, alvo)
-
 if resultado != -1:
     print()
 else:
